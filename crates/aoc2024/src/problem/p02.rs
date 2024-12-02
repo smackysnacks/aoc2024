02568d68fcad9b821d40c519f1759c865bff0565
Mack Stump <mack@mackstump.com>
2024-12-02 14:35:17 -0500

    day 2 simplify

--- crates/aoc2024/src/problem/p02.rs
+++ crates/aoc2024/src/problem/p02.rs
@@ -1,67 +1,35 @@
 use aoc2024_core::iter_lines;
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-enum Direction {
-    Increasing,
-    Decreasing,
-    Invalid,
+fn check(report: &[i32]) -> bool {
+    let diff: Vec<_> = report.windows(2).map(|pair| pair[0] - pair[1]).collect();
+    diff.iter().all(|x| (1..=3).contains(x)) || diff.iter().all(|x| (-3..0).contains(x))
 }
 
-fn check<'a>(mut report: impl Iterator<Item = &'a i32>) -> bool {
-    let mut prev = report.next().unwrap();
-    let mut it = report.map(|next| {
-        let direction = match next - prev {
-            1..=3 => Direction::Increasing,
-            -3..=-1 => Direction::Decreasing,
-            _ => Direction::Invalid,
-        };
-        prev = next;
-        direction
-    });
-    let direction = it.next().unwrap();
-    let result = it.fold(direction, |expected, direction| {
-        match expected != direction || direction == Direction::Invalid {
-            true => Direction::Invalid,
-            false => direction,
-        }
-    });
-    result != Direction::Invalid
-}
-
-pub fn solve() {
-    let input = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/p02.in"));
-
-    let reports = iter_lines(input)
-        .map(|line| {
-            line.split_whitespace()
-                .map(|num| num.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>()
-        })
-        .collect::<Vec<_>>();
-
+fn part1(reports: &[Vec<i32>]) {
     let valid = reports
         .iter()
-        .map(|report| {
-            let diff: Vec<_> = report.windows(2).map(|pair| pair[0] - pair[1]).collect();
-            diff.iter().all(|x| (1..=3).contains(x)) || diff.iter().all(|x| (-3..0).contains(x))
-        })
+        .map(|report| check(report))
         .filter(|&x| x)
         .count();
     println!("p02 part 1: {}", valid);
+}
 
+fn part2(reports: Vec<Vec<i32>>) {
     let mut sum = 0;
-    for report in reports {
-        if check(report.iter()) {
+    for mut report in reports {
+        if check(&report) {
             sum += 1;
             continue;
         }
 
         let mut valid = false;
         for i in 0..report.len() {
-            if check(report[0..i].iter().chain(&report[i + 1..])) {
+            let removed = report.remove(i);
+            if check(&report) {
                 valid = true;
                 break;
             }
+            report.insert(i, removed);
         }
 
         if valid {
@@ -69,4 +37,19 @@ pub fn solve() {
         }
     }
     println!("p02 part 2: {}", sum);
+}
+
+pub fn solve() {
+    let input = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/p02.in"));
+
+    let reports: Vec<Vec<i32>> = iter_lines(input)
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+
+    part1(&reports);
+    part2(reports);
 }
