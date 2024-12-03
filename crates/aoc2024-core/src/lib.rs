ef3897387620fd6207f8d27222975c8b0ce52491
Mack Stump <mack@mackstump.com>
2024-12-03 09:38:08 -0500

    day 3

--- crates/aoc2024-core/src/lib.rs
+++ crates/aoc2024-core/src/lib.rs
@@ -1,11 +1,17 @@
 pub mod counter;
 
+use std::str::FromStr;
+
 pub use counter::*;
 
-pub fn iter_lines<T: AsRef<str> + ?Sized>(s: &T) -> impl Iterator<Item = &str> {
+pub fn lines_iter<T: AsRef<str> + ?Sized>(s: &T) -> impl Iterator<Item = &str> {
     s.as_ref().lines()
 }
 
 pub fn lines(s: &str) -> Vec<String> {
     s.lines().map(|s| s.into()).collect()
+}
+
+pub fn parse<F: FromStr<Err: std::fmt::Debug>>(s: &str) -> F {
+    s.parse().unwrap()
 }
--- crates/aoc2024/src/main.rs
+++ crates/aoc2024/src/main.rs
@@ -3,4 +3,5 @@ mod problem;
 fn main() {
     problem::p01::solve();
     problem::p02::solve();
+    problem::p03::solve();
 }
